c7ac24adb17271b9e225cdc739041d7a11f7dc42
Alex Shen <shen.alexander@gmail.com>
2021-11-10 17:20:24 -0500

    Initial project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Setup Instructions
+- Install Node.js on your computer (I used v16.13.0 to set up this repo)
+- Enter `npm install` into terminal - this will install the necessary dependencies (i.e. the Pokémon Showdown Node package)
+- Run the program by running `node bot.js` in terminal (currently doesn't do anything)
---
+++ bot.js
@@ -0,0 +1,12 @@
+const Sim = require('pokemon-showdown');
+stream = new Sim.BattleStream();
+
+(async () => {
+    for await (const output of stream) {
+        console.log(output);
+    }
+})();
+
+stream.write(`>start {"formatid":"gen7randombattle"}`);
+stream.write(`>player p1 {"name":"Alice"}`);
+stream.write(`>player p2 {"name":"Bob"}`);
