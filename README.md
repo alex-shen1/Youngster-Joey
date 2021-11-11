f8b566fa1986bad3bc8aaa9d03537e9dab0c0b6c
Alex Shen <shen.alexander@gmail.com>
2021-11-11 16:49:30 -0500

    Some investigation/scratch work

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 - Install Node.js on your computer (I used v16.13.0 to set up this repo)
 - Enter `npm install` into terminal - this will install the necessary dependencies (i.e. the Pokémon Showdown Node package)
 - Run the program by running `node bot.js` in terminal (currently doesn't do anything)
+
+# Technical Investigations
+- Interface with the battle simulator by using the `stream` object created upon initialization
+  - `stream.write()` to input commands
+  - `stream.read().then(r => {STUFF})` to get one line of output from the stream
+- 3 commands always necessary to start any battle:
+  - `>start {"formatid": "{FORMAT}"}` - initializes battle format
+  - `>player p1 {"name": "{NAME}"}` - initializes player 1
+  - `>player p2 {"name": "{NAME}"}` - initializes player 2
+  - Can add `TEAM` field to player JSON if not a random battle; doing this for testing purposes
+- After that, input `>p1 {ACTION}` and `>p2 {ACTION}` e.g. `>p1 move 1`, `p2 move 1`
+- Stream will output `sideupdate`s for both sides and an `update` of the overall game state, then wait for next input of actions 
--- bot.js
+++ bot.js
@@ -1,12 +1,50 @@
 const Sim = require('pokemon-showdown');
 stream = new Sim.BattleStream();
 
-(async () => {
-    for await (const output of stream) {
-        console.log(output);
-    }
-})();
+// (async () => {
+//     for await (const output of stream) {
+//         console.log(output);
+//     }
+// })();
 
-stream.write(`>start {"formatid":"gen7randombattle"}`);
+stream.write(`>start {"formatid":"gen1randombattle"}`);
 stream.write(`>player p1 {"name":"Alice"}`);
 stream.write(`>player p2 {"name":"Bob"}`);
+stream.read().then(r => console.log(r))
+stream.read().then(r => console.log(r))
+stream.read().then(r => console.log(r))
+stream.read().then(r => {
+    console.log(r)
+    takeTurn()
+})
+// read()
+function read(){
+    stream.read().then(r => console.log(r))
+}
+
+const prompt = require('prompt-sync')()
+
+function takeTurn(){
+    console.log('Move!')
+    const x = prompt('');
+    stream.write(`${x}`)
+    stream.read().then(r => {
+        if(r.indexOf('{') != -1 && r.lastIndexOf('}') != -1){
+            const j = JSON.stringify(JSON.parse(r.substr(r.indexOf('{'), r.lastIndexOf('}') + 1)), null, 2)
+            console.log(j)
+        }
+        console.log(r)
+    })
+    stream.read().then(r => {
+        if(r.indexOf('{') != -1 && r.lastIndexOf('}') != -1){
+            const j = JSON.stringify(JSON.parse(r.substr(r.indexOf('{'), r.lastIndexOf('}') + 1)), null, 2)
+            console.log(r.substr(0, r.indexOf('{')))
+            console.log(j)
+        }
+        else{
+            console.log(r)
+        }
+
+    })
+}
+
