3f2ab8d5bd744b5801f9909416c95dd4394a03f7
Jaden <jkylerwank@gmail.com>
2021-11-30 11:17:07 -0500

    Can have a full game with the AI

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ stream = new Sim.BattleStream();
 // Standard initialization logic
 stream.write(`>start {"formatid":"gen1randombattle"}`);
 stream.write(`>player p1 {"name":"Alice"}`);
-stream.write(`>player p2 {"name":"Bob"}`);
+stream.write(`>player p2 {"name":"Youngster Joey"}`);
 
 // Counter for how many times stream has been read
 let i = 0
@@ -19,22 +19,26 @@ const gen = Generations.get(1); // Gen 1 for damage calcs
 readUntilEnd()
 
 // Move all of this into a class structure
-p1Data = null // All of p1's sideupdate data
-p1ActiveMon = null // The active mon's info sent during side update
-p1ActiveMoves = null // The moves of the current active mon (including move, id, pp, maxpp) etc
-p1ActiveName = null // A string name of the active pokemon (used for smogon damage calc)
-p1ActiveHP = null // String of the current hp of active pokemon
-// const p1AllMon = []; // All monsters available to switch
-var p1AllMonDict = {}; // All monsters available into which p1 can switch and their indices
-var p2AllMonDict = {}; // All monsters available into which p1 can switch and their indices
+let p1Data = null // All of p1's sideupdate data
+let p1ActiveMon = null // The active mon's info sent during side update
+let p1ActiveMoves = null // The moves of the current active mon (including move, id, pp, maxpp) etc
+let p1ActiveName = null // A string name of the active pokemon (used for smogon damage calc)
+let p1ActiveHP = null // String of the current hp of active pokemon
+let p1AllMonDict = {}; // All monsters available into which p1 can switch and their indices
+let p1Wait = null // Bool if p1 currently has to wait
+let p1ForceSwitch = null // Bool if p1 currently is forced to switch
+let p1Side = null // All the "side" data within p1Data
 
-p2Data = null
-p2ActiveMon = null
-p2ActiveMonNum = 0
-p2ActiveMoves = null
-P2NonActiveMoves = null
-p2ActiveName = null
-p2ActiveHP = null
+let p2Data = null
+let p2ActiveMon = null
+let p2ActiveMonNum = 0
+let p2ActiveMoves = null
+let p2ActiveName = null
+let p2ActiveHP = null
+let p2AllMonDict = {}; // All monsters available into which p1 can switch and their indices
+let p2Wait = null
+let p2ForceSwitch = null
+let p2Side = null
 
 streamOutput = '';
 
@@ -80,52 +84,50 @@ async function read() {
         else {
             console.log(r)
         }
-        // else{
-        //      console.log(r)
-        // }
         i += 1
         if (r.indexOf('sideupdate\np1') != -1) {
             p1Data = data
-            sidepokemon = data.side.pokemon
-            for (i = 0; i < sidepokemon.length; i++) {
-                if (sidepokemon[i].active) {
-                    p1ActiveMon = sidepokemon[i]
-                    i = sidepokemon.length
+            p1Side = data.side.pokemon
+            // console.log("p1Data !!!!!!!!!!!")
+            // console.log(JSON.stringify(p1Data))
+            p1Wait = p1Data.wait
+            p1ForceSwitch = p1Data.forceSwitch
+
+            for (i = 0; i < p1Side.length; i++) {
+                if (p1Side[i].active) {
+                    p1ActiveMon = p1Side[i]
+                    i = p1Side.length
                 }
             }
             p1ActiveMoves = p1Data.active[0].moves
             p1ActiveName = p1ActiveMon.details.substr(0, p1ActiveMon.details.indexOf(','))
             p1ActiveHP = p1ActiveMon.condition.substr(0, p1ActiveMon.condition.indexOf('/'))
 
-            // List of switchable pokemon
-            // for(i=0; i < sidepokemon.length; i++) {
-            //     if (sidepokemon[i].active != "fnt") {
-            //         p1AllMon.push(sidepokemon[i].details.substr(0, sidepokemon[i].details.indexOf(',')))
-            //     }
-            // }
-            // const index = p1AllMon.indexOf(p1ActiveName);
-            // if (index > -1) {
-            // p1AllMon.splice(index, 1);
-            // }
-            // console.log(p1AllMon); 
-
             // Dictionary of switchable pokemon for p1
-            for (i = 0; i < sidepokemon.length; i++) {
-                if (sidepokemon[i].active == false) {
-                    // p1AllMon.push(sidepokemon[i].details.substr(0, sidepokemon[i].details.indexOf(',')))
-                    p1AllMonDict[i + 1] = sidepokemon[i].details.substr(0, sidepokemon[i].details.indexOf(','))
+            for (i = 0; i < p1Side.length; i++) {
+                if (p1Side[i].active == false) {
+                    if (p1Side[i].condition == "0 fnt") {
+                        p1AllMonDict[i + 1] = p1Side[i].details.substr(0, p1Side[i].details.indexOf(',')) + " (fainted)"
+                    }
+                    else {                    
+                        p1AllMonDict[i + 1] = p1Side[i].details.substr(0, p1Side[i].details.indexOf(',')) + " [" + p1Side[i].moves + "]"
+                    }
                 }
             }
 
         }
         if (r.indexOf('sideupdate\np2') != -1) {
             p2Data = data
-            sidepokemon2 = data.side.pokemon
-            for (i = 0; i < sidepokemon2.length; i++) {
-                if (sidepokemon2[i].active) {
-                    p2ActiveMon = sidepokemon2[i]
+            p2Side = data.side.pokemon
+            // console.log("p2Data !!!!!!!!!!!")
+            // console.log(JSON.stringify(p2Data))
+            p2Wait = p2Data.wait
+            p2ForceSwitch = p2Data.forceSwitch
+            for (i = 0; i < p2Side.length; i++) {
+                if (p2Side[i].active) {
+                    p2ActiveMon = p2Side[i]
                     p2ActiveMonNum = i + 1
-                    i = sidepokemon2.length
+                    i = p2Side.length
                 }
             }
             p2ActiveMoves = p2Data.active[0].moves
@@ -134,10 +136,14 @@ async function read() {
 
             //console.log(JSON.stringify(p2ActiveMon, null, 2))
             // Dictionary of switchable pokemon for p2
-            for (i = 0; i < sidepokemon2.length; i++) {
-                if (sidepokemon2[i].active == false) {
-                    // p1AllMon.push(sidepokemon2[i].details.substr(0, sidepokemon2[i].details.indexOf(',')))
-                    p2AllMonDict[i + 1] = sidepokemon2[i].details.substr(0, sidepokemon2[i].details.indexOf(','))
+            for (i = 0; i < p2Side.length; i++) {
+                if (p2Side[i].active == false) {
+                    if (p2Side[i].condition == "0 fnt") {
+                        p2AllMonDict[i + 1] = p2Side[i].details.substr(0, p2Side[i].details.indexOf(',')) + " (fainted)"
+                    }
+                    else {                    
+                        p2AllMonDict[i + 1] = p2Side[i].details.substr(0, p2Side[i].details.indexOf(','))
+                    }
                 }
             }
         }
@@ -155,84 +161,13 @@ const prompt = require('prompt-sync')()
 // Current bug exists with moves that require a recharge
 
 function takeTurn() {
-    notSwap = 0
-    // numberMon = Object.keys(p2AllMonDict).find(key => p2AllMonDict[key].active === true)
-    //estimateDamage(p1ActiveName, p1ActiveHP, p2ActiveName, p2ActiveHP, p1ActiveMoves, swap, numberMon)
-    console.log('P1 Move! Available Moves for ' + p1ActiveName + ': ')
-    const p1moves = p1Data["active"][0].moves || null
-    console.log(p1moves.map(c => c.move))
-    console.log(p1AllMonDict)
-    // console.log("p2")
-    // console.log(p2AllMonDict)
-    // clear dictionaries***************************************
-    // p1AllMon.length = 0
-    const x = prompt('');
-    if (x == "quit")
-        process.exit(0)
-    stream.write(`${x}`)
-    let bestMoves = []
-    // let decision = estimateDamage(p2ActiveName, p2ActiveHP, p1ActiveName, p1ActiveHP, p2ActiveMoves, swap, p2ActiveMonNum) // Comment this line to disable "AI"
-    // console.log(decision)
-    // bestMoves.push(decision)
-    notSwap = 0
-    for (j = 0; j < sidepokemon2.length; j++) {
-        if (sidepokemon2[j].active == false) {
-            console.log("YOU MADE IT INTO THE IF")
-            // p2Moves = []
-            // for (k = 0; k < p2Data.side.pokemon[j].moves.length; k++) {
-            //     p2Moves.push(Dex.moves.get(p2Data.side.pokemon[j].moves[k]).id)
-            // }
-            // HERE IS THE LINE WE NEED TO CHANGE
-            p2Moves = p1ActiveMoves
-            console.log(p2Moves)
-            p2Name = sidepokemon2[j].details.substr(0, sidepokemon2[j].details.indexOf(','))
-            // console.log(p2Name)
-            p2HP = sidepokemon2[j].condition.substr(0, sidepokemon2[j].condition.indexOf('/'))
-            // console.log(p2HP)
-            numberMon = j + 1
-            let decision = estimateDamage(p2Name, p2HP, p1ActiveName, p1ActiveHP, p2Moves, numberMon)
-            // console.log("HELLO " + decision)
-            bestMoves.push(decision)
-            // }
-        } else { // should just be active = true, could be f
-            let decision = estimateDamage(p2ActiveName, p2ActiveHP, p1ActiveName, p1ActiveHP, p2ActiveMoves, p2ActiveMonNum) // Comment this line to disable "AI"
-            // console.log(p2ActiveName)
-            // console.log(p2ActiveHP)
-            // console.log(p2ActiveMoves)
-            //console.log("DEBUG " + decision)
-            bestMoves.push(decision)
-            //console.log("Hiiiiiiiiiiiiiiiiiiiiiiiii")
-            notSwap = j + 1
-        }
-    }
-    // console.log(JSON.stringify(bestMoves))
-    // console.log("bestMoves: ")
-    // console.log(bestMoves)
+    
+    // Handle the Human Player's input (if you need to switch or wait)
+    handleP1Input();
 
-    console.log('P2 Move! Available Moves for ' + p2ActiveName + ': ')
-    const p2moves = p2Data["active"][0].moves || null
-    console.log(p2moves.map(c => c.move))
-    decision = bestMoves[0]
-    for (i = 1; i < bestMoves.length; i++) {
-        if (decision[2] < bestMoves[i][2]) {
-            decision = bestMoves[i]
-        }
-    }
-    // const y = prompt(''); // Uncomment this line if you want to do manual input, and comment line below
-    // iterate through bestMove and see if the best move is there or if it requires switching then add logic for move or switch
+    // Handle the AI input
+    handleP2Input();
 
-    let y = ''
-    if (notSwap)
-        y = '>p2 move ' + decision[1]
-    else {
-        var dec = p2AllMonDict[decision[0]]
-        // console.log('THIS IS IT' + dec)
-        y = '>p2 switch ' + dec
-    }
-
-    if (y == "quit")
-        process.exit(0)
-    stream.write(`${y}`)
     // Each round, the stream outputs 3 times so we just do it 3 times lol
     readUntilEnd()
 }
@@ -245,25 +180,164 @@ function takeTurn() {
  * @param {*} defender the name of the active pokemon defending
  * @param {*} activeMoves the list of active moves the attacker knows
  * @param {*} numberMon the index of the mon
+ * @param {*} nonActiveMon boolean of whether the mon is active or not, which will change how activeMoves is read
  */
-function estimateDamage(attacker, attackerHP, defender, defenderHP, activeMoves, numberMon) {
+function estimateDamage(attacker, attackerHP, defender, defenderHP, activeMoves, numberMon, nonActiveMon) {
+    console.log(activeMoves)
+    if (activeMoves[0].move == "Recharge" && activeMoves.length == 1) {
+        return [numberMon, 1, 9999] // This way AI will always pick Recharge when it has to
+    }
     // Need to figure out a good way to implement the KO chance 
     // Also need a good way to pass in the current hp of both pokemon (important for recoil) / current stat boosts
     const attackingmon = new Pokemon(gen, attacker, { curHP: attackerHP })
     const defendingmon = new Pokemon(gen, defender, { curHP: defenderHP })
     let damages = []
-    for (i = 0; i < activeMoves.length; i++) {
-        const result = calculate(
-            gen,
-            attackingmon,
-            defendingmon,
-            new Move(gen, activeMoves[i].move)
-        )
-        // Not really sure which damage values to take into account, since calculate returns a Result object that gives lots of data
-        console.log(result.fullDesc())
-        damages.push((result.range()[0] + result.range()[1]) / 2)
-        //console.log('Expected damage for ' + activeMoves[i].move + ': [' + result.range() + '] || ' + result.moveDesc())
+    if (nonActiveMon) {
+        for (i = 0; i < activeMoves.length; i++) {
+            const result = calculate(
+                gen,
+                attackingmon,
+                defendingmon,
+                new Move(gen, activeMoves[i])
+            )
+            //console.log(result.fullDesc()) This line seems to cause a rare bug and unnecessary anyway
+            damages.push((result.range()[0] + result.range()[1]) / 2)
+            //console.log('Expected damage for ' + activeMoves[i].move + ': [' + result.range() + '] || ' + result.moveDesc())
+        }
+    }
+    else {
+        for (i = 0; i < activeMoves.length; i++) {
+            const result = calculate(
+                gen,
+                attackingmon,
+                defendingmon,
+                new Move(gen, activeMoves[i].move)
+            )
+            //console.log(result.fullDesc())
+            damages.push((result.range()[0] + result.range()[1]) / 2)
+            //console.log('Expected damage for ' + activeMoves[i].move + ': [' + result.range() + '] || ' + result.moveDesc())
+        }
     }
     let moveChoice = damages.indexOf(Math.max(...damages)) // decide what move number to pick
     return [numberMon, moveChoice + 1, damages[moveChoice]]
+}
+
+function handleP1Input() {
+    if (!p1Wait && !p1ForceSwitch) {
+        console.log('P1 Move! Available Moves for ' + p1ActiveName + ': ')
+        const p1moves = p1Data["active"][0].moves || null
+        console.log(p1moves.map(c => c.move))
+        console.log(p1AllMonDict)
+        const x = prompt('');
+        if (x == "quit")
+            process.exit(0)
+        stream.write(`${x}`)
+    }
+    else if (p1Wait) {
+        console.log('P1 Waiting for AI...')
+    }
+    else if (p1ForceSwitch) {
+        console.log('P1 Needs to Switch! Available Pokemon to Switch to: ')
+        console.log(p1AllMonDict)
+        const x = prompt('');
+        if (x == "quit")
+            process.exit(0)
+        stream.write(`${x}`)
+    }
+    else {
+        console.log("Not really sure how you got here...")
+        const x = prompt('');
+        if (x == "quit")
+            process.exit(0)
+        stream.write(`${x}`)
+    }
+}
+
+function handleP2Input() {
+    if (!p2Wait && !p2ForceSwitch) {
+        let bestMoves = calcBestMoves();
+        console.log("bestMoves: ")
+        console.log(bestMoves)
+        console.log('AI Move! Available Moves for ' + p2ActiveName + ': ')
+        // iterate through bestMove and see if the best move is there or if it requires switching then add logic for move or switch
+        decision = bestMoves[0]
+        for (i = 1; i < bestMoves.length; i++) {
+            if (decision[2] < bestMoves[i][2]) {
+                decision = bestMoves[i]
+            }
+        }
+        let y = ''
+        // Means that p2 found a better damaging move from another pokemon
+        if (decision[0] != 1) {
+            //console.log('AI SWITCH TIME')
+            y = '>p2 switch ' + decision[0]
+        }
+        else {
+            y = '>p2 move ' + decision[1]
+        }
+    
+        if (y == "quit")
+            process.exit(0)
+        stream.write(`${y}`)
+    }
+    else if (p2Wait) {
+        console.log('AI Waiting for P1...')
+    }
+    else if (p2ForceSwitch) {
+        // Repeating code, can move into its own method later anyway because it will be part of the heuristic calc
+        console.log('AI Needs to Switch! Available Pokemon to Switch to: ')
+        console.log(p2AllMonDict)
+        let bestMoves = calcBestMoves();
+        console.log("bestMoves: ")
+        console.log(bestMoves)
+        // iterate through bestMove and see if the best move is there or if it requires switching then add logic for move or switch
+        decision = bestMoves[0]
+        for (i = 1; i < bestMoves.length; i++) {
+            if (decision[2] < bestMoves[i][2]) {
+                decision = bestMoves[i]
+            }
+        }
+        let y = '>p2 switch ' + decision[0]
+    
+        if (y == "quit")
+            process.exit(0)
+        stream.write(`${y}`)
+    }
+    else {
+        console.log("Not really sure how you got here... #brokenaimoment")
+        const y = prompt('');
+        if (y == "quit")
+            process.exit(0)
+        stream.write(`${y}`)
+    }
+}
+
+function calcBestMoves() {
+    let bestMoves = []
+    for (j = 0; j < p2Side.length; j++) {
+        // We must not calculate damages for fainted pokemon nor must we try to switch to them
+        if (p2Side[j].condition == "0 fnt")
+            continue
+        if (p2Side[j].active == false) {
+            console.log("Inactive Pokemon")
+            p2Moves = []
+            for (k = 0; k < p2Data.side.pokemon[j].moves.length; k++) {
+                p2Moves.push(Dex.moves.get(p2Data.side.pokemon[j].moves[k]).id)
+            }
+            //console.log(p2Moves)
+            p2Name = p2Side[j].details.substr(0, p2Side[j].details.indexOf(','))
+            // console.log(p2Name)
+            p2HP = p2Side[j].condition.substr(0, p2Side[j].condition.indexOf('/'))
+            //console.log(p2HP)
+            numberMon = j + 1
+            let decision = estimateDamage(p2Name, p2HP, p1ActiveName, p1ActiveHP, p2Moves, numberMon, true)
+            bestMoves.push(decision)
+            // }
+        } else { // should just be active = true, could be f
+            console.log("Active Pokemon")
+            let decision = estimateDamage(p2ActiveName, p2ActiveHP, p1ActiveName, p1ActiveHP, p2ActiveMoves, p2ActiveMonNum, false) // Comment this line to disable "AI"
+            bestMoves.push(decision)
+        }
+    }
+    return bestMoves
 }
